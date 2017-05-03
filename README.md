2940d0990938ff9ed66cd6721990460641d2d5ee
Paragjyoti Nath <Paragjyoti Nath>
2017-05-04 01:26:16 +0530

    Created Contract class for the database.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # StudyTimer
-This is my frist project to start with git and github.
-StudyTimer is a simple Todo list app that helps one to study efficiantly with time management and preplannig.
+This is my frist project to start with git and git-hub.
+StudyTimer is a simple Todo list app that helps one to study efficiently with time management and pre planing.
+One can set a time limit  to complete a topic
+One can save doubts and notes during studying and see them later.
---
+++ app/src/main/java/com/thousandfeeds/studytimer/database/TasksContract.java
@@ -0,0 +1,41 @@
+package com.thousandfeeds.studytimer.database;
+
+import android.provider.BaseColumns;
+
+//class containing the required constants for the database.
+
+public class TasksContract {
+
+    // To prevent someone from accidentally instantiating the contract class,
+    // make the constructor private.
+    private TasksContract() {
+    }
+
+    // Constants for Main Task table.
+    public static class TaskTable implements BaseColumns {
+
+        public final String TABLE_NAME = "Task";
+        public final String COLUMN_TASK_TITLE = "Task-title";
+        public final String COLUMN_TASK_TIME_STAMP = "Task-time-stamp";
+        public final String COLUMN_TASK_TIMER_DURATION = "Timer-duration";
+    }
+
+    // Constants for the Todo list table
+    public static class ToDoListTable implements BaseColumns {
+
+        public final String TODO_TABLE_NAME = "Todo-list";
+        public final String COLUMN_TODO_TITLE = "Todo-title";
+        public final String COLUMN_TODO_TIME_STAMP = "Todo-time-stamp";
+
+    }
+
+    // Constants for the Notes table
+    public static class NotesTable implements BaseColumns {
+
+        public final String NOTES_TABLE_NAME = "Notes";
+        public final String COLUMN_NOTES_CONTENTS = "Notes-contents";
+        public final String COLUMN_NOTES_TIME_STAMP = "Notes-time-stamp";
+    }
+
+
+}
