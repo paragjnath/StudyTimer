531c0c390662683f260409df57837832f8cf922f
Paragjyoti Nath <Paragjyoti Nath>
2017-05-04 23:59:48 +0530

    Added contstants in the contract class for the URI and some changes in the contract constant names

--- app/src/main/java/com/thousandfeeds/studytimer/database/TaskDbHelper.java
+++ app/src/main/java/com/thousandfeeds/studytimer/database/TaskDbHelper.java
@@ -18,12 +18,12 @@ public class TaskDbHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "StudyTimer.db";
 
     //Sql commend for creating all the three tables.
-    private static final String CREATE_TABLE_TASKS = "CREATE TABLE "+ TaskTable.TABLE_NAME+" ( "
-            +TaskTable._ID+" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
-            +TaskTable.COLUMN_TASK_TITLE+" TEXT NOT NULL, "
-            +TaskTable.COLUMN_TASK_TIMER_DURATION+" INTEGER, "
-            +TaskTable.COLUMN_TASK_COMPLETE_TIME+" INTEGER, "
-            +TaskTable.COLUMN_TASK_TIME_STAMP+" INTEGER );"
+    private static final String CREATE_TABLE_TASKS = "CREATE TABLE "+ TasksTable.TABLE_NAME+" ( "
+            +TasksTable._ID+" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
+            +TasksTable.COLUMN_TASK_TITLE+" TEXT NOT NULL, "
+            +TasksTable.COLUMN_TASK_TIMER_DURATION+" INTEGER, "
+            +TasksTable.COLUMN_TASK_COMPLETE_TIME+" INTEGER, "
+            +TasksTable.COLUMN_TASK_TIME_STAMP+" INTEGER );"
             ;
 
     private static final String CREATE_TABLE_TODO_LIST ="CREATE TABLE "+ ToDoListTable.TODO_TABLE_NAME+" ( "
@@ -32,16 +32,16 @@ public class TaskDbHelper extends SQLiteOpenHelper {
             +ToDoListTable.COLUMN_TODO_TASK_ID+" INTEGER, "
             +ToDoListTable.COLUMN_TODO_TIME_STAMP+" INTEGER, FOREIGN KEY( "
             +ToDoListTable.COLUMN_TODO_TASK_ID+") REFERENCES "
-            +TaskTable.TABLE_NAME+"("+TaskTable._ID+") ON DELETE NO ACTION ON UPDATE NO ACTION );"
+            +TasksTable.TABLE_NAME+"("+TasksTable._ID+") ON DELETE NO ACTION ON UPDATE NO ACTION );"
             ;
 
     private static final String CREATE_TABLE_NOTES = "CREATE TABLE "+NotesTable.NOTES_TABLE_NAME+" ( "
             +NotesTable._ID+" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
-            +NotesTable.COLUMN_NOTES_CONTENTS+" TEXT NOT NULL, "
-            +NotesTable.COLUMN_NOTES_TASK_ID+" INTEGER, "
-            +NotesTable.COLUMN_NOTES_TIME_STAMP+" INTEGER, FOREIGN KEY( "
-            +NotesTable.COLUMN_NOTES_TASK_ID+") REFERENCES "
-            +TaskTable.TABLE_NAME+"("+TaskTable._ID+") ON DELETE NO ACTION ON UPDATE NO ACTION );"
+            +NotesTable.COLUMN_NOTE_CONTENTS+" TEXT NOT NULL, "
+            +NotesTable.COLUMN_NOTE_TASK_ID+" INTEGER, "
+            +NotesTable.COLUMN_NOTE_TIME_STAMP+" INTEGER, FOREIGN KEY( "
+            +NotesTable.COLUMN_NOTE_TASK_ID+") REFERENCES "
+            +TasksTable.TABLE_NAME+"("+TasksTable._ID+") ON DELETE NO ACTION ON UPDATE NO ACTION );"
             ;
 
     public TaskDbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
--- app/src/main/java/com/thousandfeeds/studytimer/database/TasksContract.java
+++ app/src/main/java/com/thousandfeeds/studytimer/database/TasksContract.java
@@ -1,20 +1,35 @@
 package com.thousandfeeds.studytimer.database;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 //class containing the required constants for the database.
 
 public final class TasksContract {
 
+    public static final String CONTENT_AUTHORITY = "com.thousandfeeds.studytimer";
+
+    /** we concatenate the CONTENT_AUTHORITY constant with the scheme
+     * “content://” we will create the BASE_CONTENT_URI which will be shared by every URI associated with TasksContract */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_TODO_LIST = "todolist";
+    public static final String PATH_NOTES = "notes";
+    public static final String PATH_TASKS = "tasks";
+
     // To prevent someone from accidentally instantiating the contract class,
     // make the constructor private.
     private TasksContract() {
     }
 
-    // Constants for Main Task table.
-    static class TaskTable implements BaseColumns {
 
-        static final String TABLE_NAME = "Task";
+    // Constants for Main Task table.
+    static class TasksTable implements BaseColumns {
+
+        /** The content URI to access the Task data in the provider */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_TASKS);
+
+        static final String TABLE_NAME = "Tasks";
         static final String COLUMN_TASK_TITLE = "Task-title";
         static final String COLUMN_TASK_TIMER_DURATION = "Timer-duration";
         static final String COLUMN_TASK_COMPLETE_TIME = "Task-completion-time";
@@ -35,6 +50,8 @@ public final class TasksContract {
 
     // Constants for the Todo list table
     static class ToDoListTable implements BaseColumns {
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_TODO_LIST);
 
         static final String TODO_TABLE_NAME = "Todo-list";
         static final String COLUMN_TODO_TITLE = "Todo-title";
@@ -57,10 +74,12 @@ public final class TasksContract {
     // Constants for the Notes table
     static class NotesTable implements BaseColumns {
 
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_NOTES);
+
         static final String NOTES_TABLE_NAME = "Notes";
-        static final String COLUMN_NOTES_CONTENTS = "Notes-contents";
-        static final String COLUMN_NOTES_TASK_ID = "Task-id";
-        static final String COLUMN_NOTES_TIME_STAMP = "Notes-time-stamp";
+        static final String COLUMN_NOTE_CONTENTS = "Note-content";
+        static final String COLUMN_NOTE_TASK_ID = "Task-id";
+        static final String COLUMN_NOTE_TIME_STAMP = "Note-time-stamp";
 
         /*
          CREATE TABLE Notes (
