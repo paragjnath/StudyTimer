a32d8c8caf9a1f4f84b748393f1968fe4a8aee01
paragjnath <paragjnath@gmail.com>
2017-05-05 15:42:46 +0530

    create sqlite database instance

--- app/src/main/java/com/thousandfeeds/studytimer/StudyTimerHome.java
+++ app/src/main/java/com/thousandfeeds/studytimer/StudyTimerHome.java
@@ -1,7 +1,10 @@
 package com.thousandfeeds.studytimer;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.thousandfeeds.studytimer.database.TaskDbHelper;
 
 public class StudyTimerHome extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class StudyTimerHome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study_timer_home);
+
+        //Added following lines to test the creation of the database at the first launch.
+        TaskDbHelper mTaskDbHelper = new TaskDbHelper(this);
+        SQLiteDatabase database = mTaskDbHelper.getReadableDatabase();
     }
 }
